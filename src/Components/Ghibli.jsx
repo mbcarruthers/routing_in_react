5728f04e49c4a6e7453e554b867ec1a8e9da421c
mbcarruthers <mbcarruthers@crimson.ua.edu>
2019-07-13 17:09:46 -0500

    added the ghibli class and inserted it into the Home component

---
+++ src/Components/Ghibli.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Ghibli = (props) => {
+    return(
+        <div className="container-fluid">
+            <img src={"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ba6eb16e-7880-45c2-8b26-10db799d9085/d8b80js-2dfedb77-dcb9-42c2-b1c9-241d0bc91866.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2JhNmViMTZlLTc4ODAtNDVjMi04YjI2LTEwZGI3OTlkOTA4NVwvZDhiODBqcy0yZGZlZGI3Ny1kY2I5LTQyYzItYjFjOS0yNDFkMGJjOTE4NjYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.i15zWhfEpDd_x4uediN_oadMT7IqZVRr3pa4FDcVz0o"} />
+        </div>
+    )
+};
+
+export default Ghibli;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Ghibli from "./Ghibli.jsx";
 
 const Home = ( props ) => {
     return(
         <div className="container-fluid text-center">
             <h4>Home</h4>
+            <Ghibli />
         </div>
     )
 };
